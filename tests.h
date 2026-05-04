b152684172e497712d162270aad241c21f3fbd39
zedzee <carsonuderitz@gmail.com>
2026-05-04 05:23:55 -0400

    improve formatting

--- tests.h
+++ tests.h
@@ -19,20 +19,20 @@ typedef enum {
 	case_status = PASS;\
 	test_case_name = STRINGIFY(name);\
 	assertion_count = 0;\
-	assert_fail_count = 0;\
+	assert_pass_count = 0;\
 	case_count++;\
-	printf(CLR_BOLD "\n	[ %s ] ( %s ) \n" CLR_RESET, test_name, test_case_name);\
+	printf(CLR_BOLD "\n	( %s ) \n" CLR_RESET, test_name, test_case_name);\
 	do {\
 		block\
 	} while(0);\
 	switch (case_status) {\
 		case FAILURE:\
-			printf(CLR_RED CLR_BOLD "	[ %s ] ( %s ) failed (%d/%d)\n" CLR_RESET, test_name, test_case_name, assert_fail_count, assertion_count);\
+			printf(CLR_RED CLR_BOLD "	( %s ) failed (%d/%d)\n" CLR_RESET, test_name, test_case_name, assert_pass_count, assertion_count);\
 			status = FAILURE;\
-			fail_count++;\
 			break;\
 		case PASS:\
-			printf(CLR_GREEN CLR_BOLD "	[ %s ] ( %s ) passed\n" CLR_RESET, test_name, test_case_name);\
+			printf(CLR_GREEN CLR_BOLD "	( %s ) passed\n" CLR_RESET, test_name, test_case_name);\
+			pass_count++;\
 			break;\
 	}
 
@@ -51,16 +51,16 @@ typedef enum {
 		(void)test_case_name;\
 		int case_count = 0;\
 		(void)case_count;\
-		int fail_count = 0;\
-		(void)fail_count;\
+		int pass_count = 0;\
+		(void)pass_count;\
 		int assertion_count = 0;\
 		(void)assertion_count;\
-		int assert_fail_count = 0;\
-		(void)assert_fail_count;\
+		int assert_pass_count = 0;\
+		(void)assert_pass_count;\
 		block\
 		switch (status) {\
 			case FAILURE:\
-						 printf(CLR_RED CLR_BOLD "\n[ %s ] failed (%d/%d)\n" CLR_RESET, STRINGIFY(name), fail_count, case_count);\
+						 printf(CLR_RED CLR_BOLD "\n[ %s ] failed (%d/%d)\n" CLR_RESET, STRINGIFY(name), pass_count, case_count);\
 			break;\
 			case PASS:\
 					  printf(CLR_GREEN CLR_BOLD "\n[ %s ] passed\n" CLR_RESET, STRINGIFY(name));\
@@ -69,15 +69,19 @@ typedef enum {
 		return 0;\
 	}
 
-#define ASSERT(condition, message)\
+#define _ASSERT(condition, message, silent)\
 	result = condition;\
 	assertion_count++;\
-	if (result == 1) {\
-		printf(CLR_GREEN "		[ %s ] ( %s ) -- Assertion %s passed\n" CLR_RESET, test_name, test_case_name, STRINGIFY(condition));\
+	if (result) {\
+		if (!silent) {\
+			printf(CLR_GREEN "		-- Assertion %s passed\n" CLR_RESET, test_name, test_case_name, STRINGIFY(condition));\
+		}\
+		assert_pass_count++;\
 	} else {\
-		assert_fail_count++;\
-		printf(CLR_RED "		[ %s ] ( %s ) -- Assertion %s failed: %s\n" CLR_RESET, test_name, test_case_name, STRINGIFY(condition), message);\
+		printf(CLR_RED "		-- Assertion %s failed: %s\n" CLR_RESET, test_name, test_case_name, STRINGIFY(condition), message);\
 		case_status = FAILURE;\
 	}
+#define ASSERT(condition, message) _ASSERT(condition, message, 1);
+#define ASSERT_SILENT(condition, message) _ASSERT(condition, message, 0);
 
 #endif
